323c29762966f99fdde98ade4aaa4aad66bf7ef3
laravel <abbas.ajorlou1371@gmail.com>
2023-01-23 01:55:42 +0330

    Ip range import functionality added

--- app/Http/Livewire/IpManagement/ApiIpRanges.php
+++ app/Http/Livewire/IpManagement/ApiIpRanges.php
@@ -7,15 +7,18 @@ use Illuminate\Support\Facades\Cache;
 use App\Helpers\SMS;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Livewire\WithFileUploads;
 use Morilog\Jalali\Jalalian;
 
 class ApiIpRanges extends Component
 {
+    use WithFileUploads;
+
     public $ip_ranges = [],
         $ip_range = [],
         $starting_ip = [],
         $ending_ip = [],
-        $title, $code, $accessPassword, $admin;
+        $title, $code, $accessPassword, $admin, $file;
 
     protected $listeners = [
         'ipRangeCreated' => '$refresh',
@@ -45,6 +48,7 @@ class ApiIpRanges extends Component
         'ending_ip.*.min' => 'مقدار صحیح نمی باشد',
         'ending_ip.*.max' => 'مقدار صحیح نمی باشد',
         'accessPassword.required' => 'رمز دسترسی را وارد کنید',
+
     ];
 
     public function mount()
@@ -76,6 +80,7 @@ class ApiIpRanges extends Component
                 session()->flash('success', $r->statustext);
             }
         } else {
+            Cache::clear('ips-verify-code-' . $this->admin->id);
             session()->flash('error', explode(":", $result)[1]);
         }
     }
@@ -128,6 +133,76 @@ class ApiIpRanges extends Component
         }
     }
 
+    public function import()
+    {
+        $this->validate(
+            [
+                'file' => 'required|file|mimes:txt',
+                'code' => 'required|integer',
+                'accessPassword' => 'required',
+                'title' => 'required|string'
+            ],
+            [
+                'accessPassword.required' => 'رمز دسترسی را وارد کنید',
+                'file.required' => 'فایل را بارگذاری کنید',
+                'file.mimes' => 'فرمت فایل باید .txt باشد.',
+                'file.file' => 'فرمت فایل صحیح نیست.',
+                'title.required' => 'عنوان را وارد کنید',
+                'code.required' => 'کد تایید را وارد کنید',
+                'code.integer' => 'کد تایید صحیح نمی باشد',
+                'code.min' => 'کد تایید صحیح نمی باشد',
+                'accessPassword.required' => 'رمز دسترسی را وارد کنید',
+                'title.required' => 'عنوان را وارد کنید',
+            ],
+            ['file', 'code', 'accessPassword', 'title']
+        );
+
+        $cachedCode = Cache::get('ips-verify-code-' . $this->admin->id);
+
+        if (!$cachedCode || !Hash::check($this->code, $cachedCode)) {
+            $this->addError('code', 'کد تایید وارد شده صحیح نیست');
+        } else if (!Hash::check($this->accessPassword, $this->admin->access_password)) {
+            $this->addError('accessPassword', 'رمز دسترسی صحیح نیست');
+        } else {
+
+            $fileName =  $this->file->getClientOriginalName();
+
+            $this->file->storePubliclyAs('ip-ranges', $fileName, 'public');
+
+            $file = file_get_contents(public_path('storage/ip-ranges/' . $fileName));
+
+            $imported_ips = preg_split('/\s+/', $file);
+            $imported_ips = implode('-', $imported_ips);
+            $imported_ips = explode('-', $imported_ips);
+
+            if (file_exists(storage_path('/ip-management/ips.json'))) {
+                $ips = file_get_contents(storage_path('/ip-management/ips.json'));
+                $ips = json_decode($ips, true);
+            } else {
+                $ips = [];
+            }
+
+            for ($i = 0; $i < count($imported_ips) - 1; $i += 2) {
+                $this->ip_range = [
+                    'title' => $this->title,
+                    'starting_ip' => $imported_ips[$i],
+                    'ending_ip' => $imported_ips[$i + 1],
+                    'created_date' => Jalalian::forge(now())->format('Y/m/d'),
+                    'created_hour' => Jalalian::forge(now())->format('H:m:s'),
+                    'created_by' => $this->admin->name,
+                ];
+                array_push($this->ip_ranges, $this->ip_range);
+            }
+
+            $ips['ip_ranges'] = $this->ip_ranges;
+            file_put_contents(storage_path('/ip-management/ips.json'), json_encode($ips));
+            session()->flash('success', 'درون ریزی با موفقیت انجام شد.');
+            $this->reset(['code', 'accessPassword', 'file', 'title']);
+            Cache::delete('ips-verify-code-' . $this->admin->id);
+            $this->emitSelf('ipRangeCreated');
+        }
+    }
+
     public function updated($prop)
     {
         $this->validateOnly($prop);
@@ -146,10 +221,25 @@ class ApiIpRanges extends Component
         session()->flash('success', 'آی پی حذف شد');
     }
 
+    public function flushIpRanges()
+    {
+        if (!file_exists(storage_path('ip-management/ips.json'))) return;
+
+        $ips = file_get_contents(storage_path('/ip-management/ips.json'));
+        $ips = json_decode($ips, true);
+
+        if (array_key_exists('ip_ranges', $ips)) {
+            unset($ips['ip_ranges']);
+            file_put_contents(storage_path('ip-management/ips.json'), json_encode($ips));
+            $this->emitSelf('ipRangeDeleted');
+            session()->flash('success', 'Range Ip Flushed Successfully.');
+        }
+    }
+
     public function render()
     {
         return view('livewire.ip-management.api-ip-ranges')
-        ->extends('layouts.app')
-        ->section('content');
+            ->extends('layouts.app')
+            ->section('content');
     }
 }
--- resources/views/livewire/ip-management/api-ip-ranges.blade.php
+++ resources/views/livewire/ip-management/api-ip-ranges.blade.php
@@ -1,6 +1,5 @@
 <div>
     {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
-    @can('Define-Range-IP')
     <x-buttons.btn-primary class="my-2" data-bs-toggle="modal" data-bs-target="#api-ip-range-modal">تعریف رنج IP
     </x-buttons.btn-primary>
     <x-modals.modal id="api-ip-range-modal" title="تعریف رنج آی پی Api">
@@ -10,62 +9,110 @@
         @if (session('error'))
             <x-alerts.danger>{{ session('error') }}</x-alerts.danger>
         @endif
-        <x-forms.group  for="title" label="عنوان">
-            <x-forms.input wire:model="title"/>
+        <x-forms.group for="title" label="عنوان">
+            <x-forms.input wire:model="title" />
+            @error('title')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+            </x-fomrs.gourp>
+            <x-forms.group for="starting_ip" label="آی پی شروع">
+                <div class="row">
+                    @for ($i = 0; $i < 4; $i++)
+                        <div class="col">
+                            <x-forms.input wire:model="starting_ip.{{ 3 - $i }}" />
+                            @error('starting_ip.' . (3 - $i))
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                    @endfor
+                </div>
+            </x-forms.group>
+            <x-forms.group for="ending_ip" label="آی پی پایانی">
+                <div class="row">
+                    @for ($i = 0; $i < 4; $i++)
+                        <div class="col">
+                            <x-forms.input wire:model="ending_ip.{{ 3 - $i }}" />
+                            @error('ending_ip.' . (3 - $i))
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                    @endfor
+                </div>
+            </x-forms.group>
+            <div class="form-group row">
+                <div class="col-sm-4">
+                    <x-buttons.btn-success wire:loading.attr="disabled" wire:click="sendCode">ارسال کد تایید
+                    </x-buttons.btn-success>
+                </div>
+                <div class="col-sm-8">
+                    <x-forms.input wire:model="code" />
+                    @error('code')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+            </div>
+            <x-forms.group for="access-password" label="رمز دسترسی">
+                <x-forms.input type="password" id="access-password" wire:model="accessPassword" />
+                @error('accessPassword')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </x-forms.group>
+            <x-slot name="footer">
+                <x-buttons.btn-info wire:loading.attr="disabled" wire:click="update">ثبت</x-buttons.btn-info>
+                <x-buttons.btn-danger data-bs-dismiss="modal">بستن</x-buttons.btn-danger>
+            </x-slot>
+    </x-modals.modal>
+
+    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <x-buttons.btn-primary class="my-2" data-bs-toggle="modal" data-bs-target="#import-api-ip-range-modal">درون ریزی
+        رنج آی پی
+    </x-buttons.btn-primary>
+    <x-modals.modal id="import-api-ip-range-modal" title="درون ریزی رنج آی پی">
+        @if (session('success'))
+            <x-alerts.success>{{ session('success') }}</x-alerts.success>
+        @endif
+        @if (session('error'))
+            <x-alerts.danger>{{ session('error') }}</x-alerts.danger>
+        @endif
+        <x-forms.group for="import-title" label="عنوان">
+            <x-forms.input wire:model="title" id="import-file" />
             @error('title')
                 <span class="text-danger">{{ $message }}</span>
             @enderror
         </x-fomrs.gourp>
-        <x-forms.group for="starting_ip" label="آی پی شروع">
-            <div class="row">
-                @for ($i = 0; $i < 4; $i++)
-                    <div class="col">
-                        <x-forms.input wire:model="starting_ip.{{ 3 - $i }}" />
-                        @error('starting_ip.' . (3 - $i))
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-                @endfor
-            </div>
-        </x-forms.group>
-        <x-forms.group for="ending_ip" label="آی پی پایانی">
-            <div class="row">
-                @for ($i = 0; $i < 4; $i++)
-                    <div class="col">
-                        <x-forms.input wire:model="ending_ip.{{ 3 - $i }}" />
-                        @error('ending_ip.' . (3 - $i))
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-                @endfor
-            </div>
-        </x-forms.group>
-        <div class="form-group row">
-            <div class="col-sm-4">
-                <x-buttons.btn-success wire:loading.attr="disabled" wire:click="sendCode">ارسال کد تایید
-                </x-buttons.btn-success>
-            </div>
-            <div class="col-sm-8">
-                <x-forms.input wire:model="code" />
-                @error('code')
-                    <span class="text-danger">{{ $message }}</span>
-                @enderror
-            </div>
-        </div>
-        <x-forms.group for="access-password" label="رمز دسترسی">
-            <x-forms.input type="password" id="access-password" wire:model="accessPassword" />
-            @error('accessPassword')
+        <x-forms.group for="import-file" label="فایل">
+            <x-forms.input type="file" wire:model="file" id="import-file" />
+            <span class="text-success" wire:loading wire:target="file">در حال بارگذاری ...</span>
+            @error('file')
                 <span class="text-danger">{{ $message }}</span>
             @enderror
-        </x-forms.group>
-        <x-slot name="footer">
-            <x-buttons.btn-info wire:loading.attr="disabled" wire:click="update">ثبت</x-buttons.btn-info>
-            <x-buttons.btn-danger data-bs-dismiss="modal">بستن</x-buttons.btn-danger>
-        </x-slot>
+        </x-fomrs.gourp>
+            <div class="form-group row">
+                <div class="col-sm-4">
+                    <x-buttons.btn-success wire:loading.attr="disabled" wire:click="sendCode">ارسال کد تایید
+                    </x-buttons.btn-success>
+                </div>
+                <div class="col-sm-8">
+                    <x-forms.input wire:model="code" />
+                    @error('code')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+            </div>
+            <x-forms.group for="access-password" label="رمز دسترسی">
+                <x-forms.input type="password" id="access-password" wire:model="accessPassword" />
+                @error('accessPassword')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </x-forms.group>
+            <x-slot name="footer">
+                <x-buttons.btn-info wire:loading.attr="disabled" wire:click="import">ثبت</x-buttons.btn-info>
+                <x-buttons.btn-danger data-bs-dismiss="modal">بستن</x-buttons.btn-danger>
+            </x-slot>
     </x-modals.modal>
-    @endcan
+    <x-buttons.btn-danger wire:click="flushIpRanges">Flush</x-buttons.btn-danger>
     @if (count($ip_ranges) > 0)
-        <x-tables.table>
+        <x-tables.table id="ips-table">
             <x-slot name="headers">
                 <th>عنوان</th>
                 <th>از آی پی</th>
@@ -94,4 +141,15 @@
     @else
         <x-alerts.danger>رنچ آی پی تعریف نشده است</x-alerts.danger>
     @endif
+    @push('js')
+        <script>
+            tableMain = $('#ips-table').DataTable({
+                "columnDefs": [{
+                    "targets": 4,
+                    "orderable": false
+                }],
+                "pageLength": 10
+            });
+        </script>
+    @endpush
 </div>
