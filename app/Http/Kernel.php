9db35dcdad7af9a81624da6ed5165f835cfb0182
abbas <abbas.ajorlou1371@gmail.com>
2023-01-31 17:28:48 +0330

    Admin ip middleware small bug fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,7 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'check.ip' => \App\Http\Middleware\BlockIpMiddleware::class,
         // ...
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
