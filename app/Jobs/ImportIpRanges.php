ddcd1e118127630cdd6aa54e79ff44a3dcfd9d7c
admin <admin@info.com>
2023-02-02 11:57:34 +0330

    sfsd

--- app/Jobs/ImportIpRanges.php
+++ app/Jobs/ImportIpRanges.php
@@ -10,7 +10,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use App\Models\Ip;
 
-class ImportIpRanges implements ShouldQueue, ShouldBeUnique
+class ImportIpRanges implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
