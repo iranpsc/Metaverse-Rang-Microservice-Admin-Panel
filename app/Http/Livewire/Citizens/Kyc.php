7afbe3fba17a051dbfc2a10e34f06d0d3bffb900
abbas <abbas.ajorlou1371@gmail.com>
2023-03-02 14:30:23 +0330

    Kyc order bug fix

--- app/Http/Livewire/Citizens/Kyc.php
+++ app/Http/Livewire/Citizens/Kyc.php
@@ -23,7 +23,9 @@ class Kyc extends Component
     public function render()
     {
         return view('livewire.citizens.kyc', [
-            'kycs' => $this->kycs ?? ModelKyc::with(['errors', 'user'])->paginate(10)
+            'kycs' => $this->kycs ?? ModelKyc::with(['errors', 'user'])
+            ->orderByDesc('created_at')
+            ->paginate(10)
         ])
         ->extends('layouts.app')
         ->section('content');
