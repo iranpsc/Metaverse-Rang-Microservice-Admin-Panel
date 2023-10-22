675a7c811bca930615ad0827f4810880ac5261d2
Abbas <abbas.ajorlou1371@gmail.com>
2023-10-22 17:34:58 +0330

    Small bug fix

--- app/Http/Livewire/Versions.php
+++ app/Http/Livewire/Versions.php
@@ -21,7 +21,7 @@ class Versions extends LivewireComponent
         'title' => 'required|string|max:255',
         'content' => 'required|string|max:20000',
         'versionTitle' => 'required|string|max:255',
-        'startsAt' => 'required|shamsi_date',
+        'startsAt' => 'required|date',
     ];
 
     public function store()
