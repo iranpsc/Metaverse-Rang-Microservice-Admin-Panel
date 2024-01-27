b8acecc84a1c7b16fd910dc18e457e373753623b
Abbas <abbas.ajorlou1371@gmail.com>
2024-01-27 15:18:11 +0330

    Kyc small bug fix

--- app/Http/Livewire/Citizens/Kyc.php
+++ app/Http/Livewire/Citizens/Kyc.php
@@ -20,13 +20,14 @@ class Kyc extends Component
     {
         $this->kycs = ModelKyc::with(['errors', 'user'])
             ->where('melli_code', 'like', '%' . $this->searchTerm . '%')
+            ->orderByDesc('created_at')
             ->paginate(10);
     }
 
     public function render()
     {
         return view('livewire.citizens.kyc', [
-            'kycs' => $this->kycs ?? ModelKyc::with(['errors', 'user'])->paginate(10)
+            'kycs' => $this->kycs ?? ModelKyc::with(['errors', 'user'])->orderByDesc('created_at')->paginate(10)
         ]);
     }
 }
