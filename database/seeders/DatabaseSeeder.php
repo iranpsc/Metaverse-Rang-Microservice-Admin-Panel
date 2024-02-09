b7c256b22082b9dd76239e2a6e3d6abc5bfcb96e
abbas <abbas.ajorlou1371@gmail.com>
2024-02-09 13:44:25 +0330

    Remove unused code in DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Video;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,10 +16,5 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\/User::factory()->count(1)->create();
-        Video::all()->each(function ($video) {
-            $video->update([
-                'slug' => Str::random(10),
-            ]);
-        });
     }
 }
