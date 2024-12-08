4991e2bce820441d5512436717285a7eb08e7618
abbass <abbas.ajorlou1371@gmail.com>
2024-12-08 14:36:01 +0330

    Refactor name formatting in TranslationFieldSeeder and remove unique_id assignment logic

--- database/seeders/TranslationFieldSeeder.php
+++ database/seeders/TranslationFieldSeeder.php
@@ -16,23 +16,23 @@ class TranslationFieldSeeder extends Seeder
     {
         TranslationField::chunkById(100, function ($fields) {
             foreach ($fields as $field) {
-                $field->name = Str::slug(trim($field->name));
+                $field->name = str_replace('-', ' ', Str::lower($field->name));
                 $field->save();
             }
         });
 
-        $fieldsWithDistinctNames = TranslationField::select('name')->distinct()->get();
-        $uniqueId = 1;
+        // $fieldsWithDistinctNames = TranslationField::select('name')->distinct()->get();
+        // $uniqueId = 1;
 
-        foreach ($fieldsWithDistinctNames as $field) {
-            $fields = TranslationField::where('name', $field->name)->get();
+        // foreach ($fieldsWithDistinctNames as $field) {
+        //     $fields = TranslationField::where('name', $field->name)->get();
 
-            foreach ($fields as $field) {
-                $field->unique_id = $uniqueId;
-                $field->save();
-            }
+        //     foreach ($fields as $field) {
+        //         $field->unique_id = $uniqueId;
+        //         $field->save();
+        //     }
 
-            $uniqueId++;
-        }
+        //     $uniqueId++;
+        // }
     }
 }
