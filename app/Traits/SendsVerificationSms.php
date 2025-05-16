3b5d3a05dad790f28dcc06cd9de413f76bb6e799
abbass <abbas.ajorlou1371@gmail.com>
2025-05-16 18:55:38 +0330

    Enhance SendsVerificationSms trait to assign authenticated admin and send verification SMS notifications

--- app/Traits/SendsVerificationSms.php
+++ app/Traits/SendsVerificationSms.php
@@ -2,25 +2,29 @@
 
 namespace App\Traits;
 
+use App\Models\Admin;
 use App\Notifications\SendVerificationCode;
-
+use Illuminate\Support\Facades\Auth;
 trait SendsVerificationSms
 {
     public $phone_verification;
 
     public $access_password;
 
-    public $admin;
+    public Admin $admin;
 
     public $countdownTime = 60; // in seconds
 
     public function sendSMS(string $id)
     {
+        $this->admin = Auth::user();
+
         $this->dispatch(
             'start-countdown',
             id: $id,
             countdownTime: $this->countdownTime,
         );
+
         $this->admin->notify(new SendVerificationCode);
         $this->dispatch('notify', message: 'کد تایید با موفقیت ارسال گردید');
     }
