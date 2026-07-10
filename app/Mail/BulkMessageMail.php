6a933ac86c9627e63cdc4df765689249c0ed4d34
abbass <abbas.ajorlou1371@gmail.com>
2026-07-10 21:17:59 +0330

    Implement ShouldQueue interface in BulkMessageMail class for asynchronous email processing

--- app/Mail/BulkMessageMail.php
+++ app/Mail/BulkMessageMail.php
@@ -3,13 +3,14 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Mail\Mailables\Headers;
 use Illuminate\Queue\SerializesModels;
 
-class BulkMessageMail extends Mailable
+class BulkMessageMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
